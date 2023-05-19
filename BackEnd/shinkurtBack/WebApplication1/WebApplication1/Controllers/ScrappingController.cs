1cd61adab7abb3e4540c89f76b39cbc9b0ecf63a
yisak mebrate <108977337+yisMeb@users.noreply.github.com>
2023-05-19 13:25:56 +0300

    api conplete

--- BackEnd/shinkurtBack/WebApplication1/WebApplication1/Controllers/ScrappingController.cs
+++ BackEnd/shinkurtBack/WebApplication1/WebApplication1/Controllers/ScrappingController.cs
@@ -42,5 +42,108 @@ namespace WebApplication1.Controllers
             }
             return Datalist;
         }
+        [Route("GetScrappLast")]
+        [AcceptVerbs("GET")]
+        public async Task<List<string>> GetScrappLast()
+        {
+            List<string> Datalist = new List<string>();
+            HttpClient hc = new HttpClient();
+            HttpResponseMessage result = await hc.GetAsync($"https://www.investing.com/commodities/real-time-futures");
+            Stream stream = await result.Content.ReadAsStreamAsync();
+            HtmlDocument doc = new HtmlDocument();
+            doc.Load(stream);
+            var HeaderNames = doc.DocumentNode.SelectNodes("//*[@id=\"__next\"]/div[2]/div/div/div[2]/main/div[3]/div[2]/table/tbody/tr/td[3]");
+            foreach (var item in HeaderNames)
+            {
+                Datalist.Add(item.InnerText);
+            }
+            return Datalist;
+        }
+        [Route("GetScrappHigh")]
+        [AcceptVerbs("GET")]
+        public async Task<List<string>> GetScrappHigh()
+        {
+            List<string> Datalist = new List<string>();
+            HttpClient hc = new HttpClient();
+            HttpResponseMessage result = await hc.GetAsync($"https://www.investing.com/commodities/real-time-futures");
+            Stream stream = await result.Content.ReadAsStreamAsync();
+            HtmlDocument doc = new HtmlDocument();
+            doc.Load(stream);
+            var HeaderNames = doc.DocumentNode.SelectNodes("//*[@id=\"__next\"]/div[2]/div/div/div[2]/main/div[3]/div[2]/table/tbody/tr/td[4]");
+            foreach (var item in HeaderNames)
+            {
+                Datalist.Add(item.InnerText);
+            }
+            return Datalist;
+        }
+        [Route("GetScrappLow")]
+        [AcceptVerbs("GET")]
+        public async Task<List<string>> GetScrappLow()
+        {
+            List<string> Datalist = new List<string>();
+            HttpClient hc = new HttpClient();
+            HttpResponseMessage result = await hc.GetAsync($"https://www.investing.com/commodities/real-time-futures");
+            Stream stream = await result.Content.ReadAsStreamAsync();
+            HtmlDocument doc = new HtmlDocument();
+            doc.Load(stream);
+            var HeaderNames = doc.DocumentNode.SelectNodes("//*[@id=\"__next\"]/div[2]/div/div/div[2]/main/div[3]/div[2]/table/tbody/tr/td[5]");
+            foreach (var item in HeaderNames)
+            {
+                Datalist.Add(item.InnerText);
+            }
+            return Datalist;
+        }
+        [Route("GetScrappChange")]
+        [AcceptVerbs("GET")]
+        public async Task<List<string>> GetScrappChange()
+        {
+            List<string> Datalist = new List<string>();
+            HttpClient hc = new HttpClient();
+            HttpResponseMessage result = await hc.GetAsync($"https://www.investing.com/commodities/real-time-futures");
+            Stream stream = await result.Content.ReadAsStreamAsync();
+            HtmlDocument doc = new HtmlDocument();
+            doc.Load(stream);
+            var HeaderNames = doc.DocumentNode.SelectNodes("//*[@id=\"__next\"]/div[2]/div/div/div[2]/main/div[3]/div[2]/table/tbody/tr/td[6]");
+            foreach (var item in HeaderNames)
+            {
+                Datalist.Add(item.InnerText);
+            }
+            return Datalist;
+        }
+        [Route("GetScrappChangePercent")]
+        [AcceptVerbs("GET")]
+        public async Task<List<string>> GetScrappChangePercent()
+        {
+            List<string> Datalist = new List<string>();
+            HttpClient hc = new HttpClient();
+            HttpResponseMessage result = await hc.GetAsync($"https://www.investing.com/commodities/real-time-futures");
+            Stream stream = await result.Content.ReadAsStreamAsync();
+            HtmlDocument doc = new HtmlDocument();
+            doc.Load(stream);
+            var HeaderNames = doc.DocumentNode.SelectNodes("//*[@id=\"__next\"]/div[2]/div/div/div[2]/main/div[3]/div[2]/table/tbody/tr/td[7]");
+            foreach (var item in HeaderNames)
+            {
+                Datalist.Add(item.InnerText);
+            }
+            return Datalist;
+        }
+        [Route("GetScrappChangeTime")]
+        [AcceptVerbs("GET")]
+        public async Task<List<string>> GetScrappChangeTime()
+        {
+            List<string> Datalist = new List<string>();
+            HttpClient hc = new HttpClient();
+            HttpResponseMessage result = await hc.GetAsync($"https://www.investing.com/commodities/real-time-futures");
+            Stream stream = await result.Content.ReadAsStreamAsync();
+            HtmlDocument doc = new HtmlDocument();
+            doc.Load(stream);
+            var HeaderNames = doc.DocumentNode.SelectNodes("//*[@id=\"__next\"]/div[2]/div/div/div[2]/main/div[3]/div[2]/table/tbody/tr/td[8]");
+            foreach (var item in HeaderNames)
+            {
+                Datalist.Add(item.InnerText);
+            }
+            return Datalist;
+        }
+
     }
 }
