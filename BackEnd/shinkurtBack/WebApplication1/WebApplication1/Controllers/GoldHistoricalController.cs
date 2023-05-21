48d076a3bad58e52d0d0e5bff2c90d46100876cb
yisak mebrate <108977337+yisMeb@users.noreply.github.com>
2023-05-21 12:01:35 +0300

    gold

--- BackEnd/shinkurtBack/WebApplication1/WebApplication1/Controllers/GoldHistoricalController.cs
+++ BackEnd/shinkurtBack/WebApplication1/WebApplication1/Controllers/GoldHistoricalController.cs
@@ -14,35 +14,66 @@ namespace WebApplication1.Controllers
     public class GoldHistoricalController : ControllerBase
     {
         private readonly CommiditiesDbContext _dbContext;
-            public GoldHistoricalController(CommiditiesDbContext dbContext )
-            {
-                _dbContext = dbContext;
-                
-            }
+        private readonly IConfiguration _configuration;
 
-            [HttpPost("insert-csv")]
-            public IActionResult InsertDataFromCsv(IFormFile file)
-            {
-                if (file == null || file.Length <= 0)
-                    return BadRequest("No file uploaded.");
-
-                try
-                {
-                    using (var reader = new StreamReader(file.OpenReadStream()))
-                    using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
-                    {
-                        var records = csv.GetRecords<GoldHistory>().ToList();
-
-                       _dbContext.GoldHistories.AddRange(records);
-                        _dbContext.SaveChanges();
-
-                        return Ok(records);
-                    }
-                }
-                catch (Exception ex)
-                {
-                    return StatusCode(500, $"An error occurred while inserting data: {ex.Message}");
-                }
-            }
+        public GoldHistoricalController(CommiditiesDbContext dbContext, IConfiguration configuratio)
+        {
+            _dbContext = dbContext;
+            _configuration = configuratio;
         }
+        [Route("GetGoldHistoryName")]
+        [AcceptVerbs("GET")]
+        public async Task<ActionResult> GetGoldHistoryName(int i)
+        {
+            List<GoldHistory> priceCommodities = new List<GoldHistory>();
+            List<GoldHistory> commodities = new List<GoldHistory>();
+            HttpClient hc = new HttpClient();
+            HttpResponseMessage result = await hc.GetAsync($"https://www.investing.com/commodities/gold-historical-data");
+            Stream stream = await result.Content.ReadAsStreamAsync();
+            HtmlDocument doc = new HtmlDocument();
+            doc.Load(stream);
+            HtmlNodeCollection rows = doc.DocumentNode.SelectNodes("//*[@id=\"__next\"]/div[2]/div/div/div[2]/main/div/div[5]/div/div/div[3]/div/table/tbody/tr");
+
+            if (rows.Count > 0)
+            {
+                GoldHistory[] p = new GoldHistory[rows.Count];
+                priceCommodities = await _dbContext.GoldHistories.ToListAsync();
+                _dbContext.GoldHistories.RemoveRange(priceCommodities);
+
+                for (int j = 0; j < rows.Count; j++)
+                {
+                    p[j] = new GoldHistory(); // Initialize each element of the array
+                }
+                int k = 0;
+                foreach (HtmlNode obj in rows)
+                {
+                    GoldHistory pcomm = new GoldHistory();
+                    HtmlNodeCollection cells = obj.SelectNodes("td");
+                    if (DateOnly.TryParse(cells[0].InnerText, out DateOnly time))
+                    {
+                        p[k].Date = time;
+                    }
+                    p[k].Price = cells[1].InnerText;
+                    p[k].Open = cells[2].InnerText;
+                    p[k].High = cells[3].InnerText;
+                    p[k].Low = cells[4].InnerText;
+                    p[k].Volume = cells[5].InnerText;
+                    p[k].changePercentage = cells[6].InnerText;
+                    commodities.Add(p[k]);
+                    k++;
+                }
+
+            }
+            else
+            {
+                return BadRequest("couldn't find the request 0");
+            }
+            foreach (GoldHistory pc in commodities)
+            {
+                await _dbContext.GoldHistories.AddAsync(pc);
+            }
+            await _dbContext.SaveChangesAsync();
+            return Ok(commodities);
+        }
+    }
 }
