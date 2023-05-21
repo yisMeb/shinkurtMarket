50bce0379afda43884fe47311273111e14dfe2c2
yisak mebrate <108977337+yisMeb@users.noreply.github.com>
2023-05-21 18:50:05 +0300

    date

--- BackEnd/shinkurtBack/WebApplication1/WebApplication1/Controllers/GoldHistoricalController.cs
+++ BackEnd/shinkurtBack/WebApplication1/WebApplication1/Controllers/GoldHistoricalController.cs
@@ -23,55 +23,27 @@ namespace WebApplication1.Controllers
         }
         [Route("GetGoldHistoryName")]
         [AcceptVerbs("GET")]
-        public async Task<ActionResult> GetGoldHistoryName(int i)
+        public async Task<ActionResult<IEnumerable<DateOnly>>> GetGoldHistoryName()
         {
-            List<GoldHistory> priceCommodities = new List<GoldHistory>();
-            List<GoldHistory> commodities = new List<GoldHistory>();
-            HttpClient hc = new HttpClient();
-            HttpResponseMessage result = await hc.GetAsync($"https://www.investing.com/commodities/gold-historical-data");
-            Stream stream = await result.Content.ReadAsStreamAsync();
-            HtmlDocument doc = new HtmlDocument();
-            doc.Load(stream);
-            HtmlNodeCollection rows = doc.DocumentNode.SelectNodes("//*[@id=\"__next\"]/div[2]/div/div/div[2]/main/div/div[5]/div/div/div[3]/div/table/tbody/tr");
+            var goldHistoryNames = await _dbContext.GoldHistories
+                .Select(g => ParseDate(g.Date))
+                .ToListAsync();
 
-            if (rows.Count > 0)
-            {
-                GoldHistory[] p = new GoldHistory[rows.Count];
-                priceCommodities = await _dbContext.GoldHistories.ToListAsync();
-                _dbContext.GoldHistories.RemoveRange(priceCommodities);
+            var dateOnlyValues = goldHistoryNames.Select(d => new DateOnly(d.Year, d.Month, d.Day));
 
-                for (int j = 0; j < rows.Count; j++)
-                {
-                    p[j] = new GoldHistory(); // Initialize each element of the array
-                }
-                int k = 0;
-                foreach (HtmlNode obj in rows)
-                {
-                    GoldHistory pcomm = new GoldHistory();
-                    HtmlNodeCollection cells = obj.SelectNodes("td");
-                    
-                    p[k].Date = cells[0].InnerText;
-                    p[k].Price = cells[1].InnerText;
-                    p[k].Open = cells[2].InnerText;
-                    p[k].High = cells[3].InnerText;
-                    p[k].Low = cells[4].InnerText;
-                    p[k].Volume = cells[5].InnerText;
-                    p[k].changePercentage = cells[6].InnerText;
-                    commodities.Add(p[k]);
-                    k++;
-                }
+            return Ok(dateOnlyValues);
+        }
 
-            }
-            else
+        private static DateTime ParseDate(string dateString)
+        {
+            var formats = new[] { "M/d/yyyy", "d/M/yyyy", "dd/MM/yyyy" };
+            foreach (var format in formats)
             {
-                return BadRequest("couldn't find the request 0");
+                if (DateTime.TryParseExact(dateString, format, CultureInfo.InvariantCulture, DateTimeStyles.None, out var result))
+                    return result.Date;
             }
-            foreach (GoldHistory pc in commodities)
-            {
-                await _dbContext.GoldHistories.AddAsync(pc);
-            }
-            await _dbContext.SaveChangesAsync();
-            return Ok(commodities);
+
+            throw new FormatException($"String '{dateString}' was not recognized as a valid date.");
         }
     }
 }
